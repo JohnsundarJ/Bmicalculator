f783d856b8da7d7a8dd26fb47f24e741ea0f5651
jjohnsundar <jjohnsundar9943@gmail.com>
2020-05-16 14:38:16 +0530

    Updates

---
+++ precache-manifest.77856c43b859b5ffb4cf18674ca0eafa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "82b33bf9b0e9d822802ec3910839cb8c",
+    "url": "/Bmicalculator/index.html"
+  },
+  {
+    "revision": "e298f788daf4ebc336fb",
+    "url": "/Bmicalculator/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "bf3a187edd85b3b86e25",
+    "url": "/Bmicalculator/static/js/2.30a2abf3.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/Bmicalculator/static/js/2.30a2abf3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e298f788daf4ebc336fb",
+    "url": "/Bmicalculator/static/js/main.877834d9.chunk.js"
+  },
+  {
+    "revision": "d44eb77a39237948cb9a",
+    "url": "/Bmicalculator/static/js/runtime-main.a344836f.js"
+  }
+]);
